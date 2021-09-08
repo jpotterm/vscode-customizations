72dd36c2f5d15270c2f7d48981ade18861620ca2
Jonathan Potter <jpotterm@gmail.com>
2021-09-08 11:12:32 -0400

    Regenerate extension with newer template

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,15 @@
 import * as vscode from 'vscode';
 
-
-export function activate(context: vscode.ExtensionContext): void {
+export function activate(context: vscode.ExtensionContext) {
     vscode.commands.executeCommand('setContext', 'listAutomaticKeyboardNavigation', false);
+
+    let disposable = vscode.commands.registerCommand('customizations.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from customizations!');
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-export function deactivate(): void {
+export function deactivate() {
     vscode.commands.executeCommand('setContext', 'listAutomaticKeyboardNavigation', true);
 }
